9f85d2b7c273951662d203b86d48ebea341c2189
Lee Hui Zhong (Top Glove TGW) <leehz@topglove.com.my>
2022-10-12 11:36:19 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:provider/provider.dart';
+
+import 'data/post_api_service.dart';
+import 'home_page.dart';
+
+void main() {
+  _setupLogging();
+  runApp(MyApp());
+}
+
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      // ignore: deprecated_member_use
+      builder: (_) => PostApiService.create(),
+      dispose: (_, PostApiService service) => service.client.dispose(),
+      create: (BuildContext context) {},
+      child: MaterialApp(
+        title: 'Material App',
+        home: HomePage(),
+      ),
+    );
+  }
+}
